24bbed71985dc0af950fdf72b9660ea2d1a7735b
Zainab Asif <zainabasifch22@gmail.com>
2025-09-08 02:11:15 +0500

    adding register api

--- Server/controllers/auth.controller.js
+++ Server/controllers/auth.controller.js
@@ -1,15 +1,21 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { userModel } from "Server/models/user.model.js";
+
 export const register = async (req, res) => {
   const { name, email, password } = req.body;
   if (name & email & password) {
-    try{
+    try {
+      const existingUser = await userModel.findOne({ email });
+      if (existingUser)
+        return res.json({ success: false, message: "user already exists" });
 
-
-        return res.json({ success: true });
+      const hashedPassword = await bcrypt.hash(password, "I am a hacker");
+      const user = new userModel({ name, email, password });
+      return res.json({ success: true });
+    } catch (error) {
+      return res.json({ success: false, message: error.message });
     }
-    catch(error){
-        return res.json({success:false, message: error.message})
-    }
-    
   }
 
   return res.json({ success: false, message: "Missing Details" });
