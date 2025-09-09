538da451b1b7f5df80eced4074bb24efe7e4f134
Zainab Asif <zainabasifch22@gmail.com>
2025-09-10 04:02:26 +0500

    completed be and started fe

---
+++ Server/controllers/user.controller.js
@@ -0,0 +1,20 @@
+import { userModel } from "../models/user.model.js";
+
+export const getUserData = async (req, res) => {
+  try {
+    const { userId } = req.body;
+    const user = await userModel.findById(userId);
+    if (!user)
+      return res.status(404).json({ success: true, message: "User not found" });
+
+    res.status(200).json({
+      success: true,
+      userData: {
+        name: user.name,
+        isAccountVerified: user.isAccountVerified,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
