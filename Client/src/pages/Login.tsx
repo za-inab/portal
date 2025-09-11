42a9e9d8f8932080686a24fe231fc5e9d7311f09
Zainab Asif <zainabasifch22@gmail.com>
2025-09-11 23:36:58 +0500

    login sign up page ui ready

--- Client/src/pages/Login.tsx
+++ Client/src/pages/Login.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import LoginForm from "../components/LoginForm";
 
 function Login() {
   return (
     <div>
-      <h1>Login page</h1>
+      <LoginForm />
     </div>
-  )
+  );
 }
 
 export default Login
